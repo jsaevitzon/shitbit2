c9c1b6b80fb776abb2567e52c2c701b72cd25e0a
jtloo <justinloo@cox.net>
2018-05-14 15:02:54 -0700

    Updated a bunch of functions

--- shitBitMyVersion/ViewController.swift
+++ shitBitMyVersion/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import CoreBluetooth
 
 
-
+let heartRateService = CBUUID(string: "0x180D");
 
 class ViewController: UIViewController {
     var cbCentralManager : CBCentralManager!;
     var heartRateSensor  : CBPeripheral!;
 
+    @IBOutlet weak var heartRateText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad();
         
@@ -61,7 +62,7 @@ extension ViewController: CBCentralManagerDelegate {
         }
         else if state == .poweredOn {
             //TODO: Make this so it only looks for heart rate monitors
-            cbCentralManager.scanForPeripherals(withServices: nil);
+            cbCentralManager.scanForPeripherals(withServices: [heartRateService]);
         }
         else if state == .poweredOff {
             
@@ -85,18 +86,30 @@ extension ViewController: CBCentralManagerDelegate {
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         
         //TODO: lookup heartRateServiceCBUUID, and update discoverServices()
-        
-        heartRateSensor.discoverServices(nil);
+        print("CONNECTED");
+        let ac = UIAlertController(title: "Connected", message: "Connected To Heartrate",
+                                   preferredStyle: .alert);
+        ac.addAction(UIAlertAction(title: "OK", style: .default));
+        self.present(ac, animated: true);
+        heartRateSensor.discoverServices([heartRateService]);
     }
     
     //Unable to connect to HRS
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-        <#code#>
+        let ac = UIAlertController(title: "Not Connected", message: "Could not connect To Heartrate",
+                                   preferredStyle: .alert);
+        ac.addAction(UIAlertAction(title: "OK", style: .default));
+        self.present(ac, animated: true);
+        return
     }
     
     //Disconnected from HRS
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        <#code#>
+        let ac = UIAlertController(title: "Disconnected", message: "Disconnected from Heartrate",
+                                   preferredStyle: .alert);
+        ac.addAction(UIAlertAction(title: "OK", style: .default));
+        self.present(ac, animated: true);
+        return
     }
     
 }
@@ -107,9 +120,30 @@ extension ViewController : CBPeripheralDelegate {
         if let services = peripheral.services {
             for serv in services {
                 print(serv);
-                
+                peripheral.discoverCharacteristics(nil, for: serv);
             }
             
         } else {return;}
+    }
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for char in characteristics {
+                if char.properties.contains(.read) {
+                    peripheral.readValue(for: char);
+                }
+                if char.properties.contains(.notify) {
+                    peripheral.setNotifyValue(true, for: char);
+                }
+                //if char.properties.contains(.write) {
+                   // peripheral.writeValue(nil, for: char)
+                // }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        
     }
 }
